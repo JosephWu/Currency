efd26c8e2b8ca0de4ea72dd4f16c32090ac3cc82
123686 <123686@gm.lhu.edu.tw>
2019-11-28 15:03:07 +0800

    Add AlertDialog

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -2,6 +2,7 @@ package com.home.currency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,16 +12,16 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static double USD2NTD = 30.77;
+    public static double USD2NTD = 30.9;
     public static double JPY2NTD = 0.2829;
     private EditText etNTD;
     private TextView tvUSD;
     private TextView tvJPY;
     private Button btGo;
 
-    private double ntdQuantity = 0;
-    private double usdQuantity = 0;
-    private double jpyQuantity = 0;
+    private double ntdAmount = 0;
+    private double usdAmount = 0;
+    private double jpyAmount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +38,51 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String tempNTD = etNTD.getText().toString();
                 if(tempNTD != null && !tempNTD.equals("")) {
-                    ntdQuantity = Double.valueOf(tempNTD);
-                    usdQuantity = ntdQuantity / USD2NTD;
-                    jpyQuantity = ntdQuantity / JPY2NTD;
-                    tvUSD.setText(Double.toString(usdQuantity));
-                    tvJPY.setText(Double.toString(jpyQuantity));
+                    ntdAmount = Double.valueOf(tempNTD);
+                    usdAmount = ntdAmount / USD2NTD;
+                    jpyAmount = ntdAmount / JPY2NTD;
+                    tvUSD.setText(myFormattedString(usdAmount));
+                    tvJPY.setText(myFormattedString(jpyAmount));
+
+                    String message = constructMessage(ntdAmount, usdAmount, jpyAmount);
+
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Result")
+                            .setMessage(message)
+                            .setPositiveButton("OK", null)
+                            .show();
                 }
                 else{
                     Toast.makeText(MainActivity.this, "Input NTD Quantity!",
                             Toast.LENGTH_SHORT).show();
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Problem")
+                            .setMessage("Please enter your NTD amount")
+                            .setPositiveButton("OK", null)
+                            .show();
                 }
+            }
+
+            private String constructMessage(double ntdAmount, double usdAmount, double jpyAmount) {
+                StringBuilder msg = new StringBuilder();
+                msg.append("Exchange rate: 1 USD = ");
+                msg.append(USD2NTD);
+                msg.append(" NTD, and 1 JPY = ");
+                msg.append(JPY2NTD);
+                msg.append(" NTD. \n\n");
+
+                msg.append(myFormattedString(ntdAmount));
+                msg.append(" NTD can exchange \n");
+                msg.append(myFormattedString(usdAmount));
+                msg.append(" USD or \n");
+                msg.append(myFormattedString(jpyAmount));
+                msg.append(" JPY.");
+                return msg.toString();
+            }
+
+
+            private String myFormattedString(double ntdAmount) {
+                return String.format("%.2f", ntdAmount);
             }
         });
 
